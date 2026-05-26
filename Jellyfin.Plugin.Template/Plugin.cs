41e74c5d6f900f932f82c2be921876143ab7e821
damien <damienpcx@gmail.com>
2026-05-26 11:01:13 +0800

    0.1

--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -7,7 +7,7 @@ using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
 
-namespace Jellyfin.Plugin.Template;
+namespace TMDBTagSync;
 
 /// <summary>
 /// The main plugin.
